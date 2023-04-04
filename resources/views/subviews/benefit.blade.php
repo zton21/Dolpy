0ae203cf22f5589d75a44200d82d7a0632e81cfc
HelpMePliz <87262213+HelpMePliz@users.noreply.github.com>
2023-04-04 14:50:21 +0700

    Benefit page

--- resources/views/subviews/benefit.blade.php
+++ resources/views/subviews/benefit.blade.php
@@ -1,2 +1,41 @@
-<div class="display-1 fw-bold text-center row p-4">Stonks</div>
-<div class="p row p-2 fs-1">Lorem ipsum dolor sit amet. Non laboriosam voluptates ut illo ipsum sed quia accusantium sed autem placeat! Sit delectus numquam et quas veniam ut galisum nihil qui libero unde ut ipsum beatae ut quam facilis aut cupiditate omnis. Et harum natus ut eaque incidunt et mollitia nihil. Ad doloremque dolor est quia delectus non galisum culpa.</div>
+<style>
+#benefit svg {
+    width:100%;
+    height:80vh;
+}
+</style>
+<div class="background position-absolute row">
+    <svg width="597" height="874" viewBox="0 0 597 874" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="165" cy="435" r="432" fill="url(#paint0_linear_71_99)"/>
+        <ellipse cx="165.5" cy="443" rx="323.5" ry="324" stroke="#D7E6FD" stroke-width="10"/>
+        <circle cx="165.5" cy="443.5" r="168.5" fill="white"/>
+        <path d="M-1.01354 33.6267C-1.01354 33.6267 58.6282 0.5 176.628 0.5C294.628 0.5 364 50.0001 364 50.0001L375 812C375 812 299.372 874 175 874C50.6282 874 5.70599 834.731 -1.01354 835.138C-7.73307 835.545 -1.01354 33.6267 -1.01354 33.6267Z" fill="url(#paint1_linear_71_99)"/>
+        <circle cx="487.5" cy="435.5" r="30.5" fill="#D7E6FD"/>
+        <circle cx="443" cy="275" r="15" fill="#D7E6FD"/>
+        <circle cx="319" cy="154" r="15" fill="#D7E6FD"/>
+        <circle cx="443" cy="610" r="15" fill="#D7E6FD"/>
+        <circle cx="319" cy="730" r="15" fill="#D7E6FD"/>
+        <defs>
+        <linearGradient id="paint0_linear_71_99" x1="225.5" y1="725" x2="311" y2="122.5" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#3980F3"/>
+        <stop offset="0.0001" stop-color="#3980F3"/>
+        <stop offset="1" stop-color="#1D407A"/>
+        </linearGradient>
+        <linearGradient id="paint1_linear_71_99" x1="364" y1="397" x2="117" y2="392" gradientUnits="userSpaceOnUse">
+        <stop stop-color="white" stop-opacity="0"/>
+        <stop offset="1" stop-color="white"/>
+        </linearGradient>
+        </defs>
+    </svg> 
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-4"></div>
+        <div class="col-8">
+            <div class="row fs-2 fw-bold mb-2">Let developers focus on code</div>
+            <div class="row fs-5 mb-4">Lorem ipsum dolor sit amet. Non laboriosam voluptates ut illo ipsum sed quia accusantium sed autem placeat! Sit delectus numquam et quas veniam ut galisum nihil qui libero unde ut ipsum beatae ut quam facilis aut cupiditate omnis. Et harum natus ut eaque incidunt et mollitia nihil. Ad doloremque dolor est quia delectus non galisum culpa.</div>
+            <div class="row fs-2 fw-bold mb-2">How it works?</div>
+            <div class="row fs-5 mb-4">Lorem ipsum dolor sit amet. Non laboriosam voluptates ut illo ipsum sed quia accusantium sed autem placeat! Sit delectus numquam et quas veniam</div>
+        </div>
+    </div>
+</div>
