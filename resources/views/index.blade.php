e0f0c65da22cc637bf3e0a9e2b7f4208388ba37b
HelpMePliz <87262213+HelpMePliz@users.noreply.github.com>
2023-04-11 15:25:56 +0700

    responsive android

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link href="{{URL::asset('css/rfs.css')}}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
@@ -12,22 +13,22 @@
 </head>
 <body data-bs-spy="scroll" data-bs-target="#navbar" data-bs-offset="0" tabindex="0">    
     @include('index.nav')
-    <div class="section" id="header">
+    <div class="section px-md-0 px-2 bg-success" id="header">
         @include('index.header')
     </div>
-    <div class="section" id="benefit">
+    <div class="section px-md-0 px-2 bg-danger" id="benefit">
         @include('index.benefit')
     </div>
-    <div class="section" id="feature">
+    <div class="section px-md-0 px-2" id="feature">
         @include('index.feature')
     </div>
-    <div class="section" id="pricing">
+    <div class="section px-md-0 px-2" id="pricing">
         @include('index.pricing')
     </div>
-    <div class="section" id="about">
+    <div class="section px-md-0 px-2" id="about">
         @include('index.about')
     </div>
-    <div class="section" id="footer">
+    <div class="section px-md-0 px-2" id="footer">
         @include('index.footer')
     </div>
 
