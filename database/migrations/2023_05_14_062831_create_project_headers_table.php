7cfb6930e53e65920267774189c252815e771142
zton21 <kartono.apriliyandi@binus.ac.id>
2023-05-14 13:35:35 +0700

    project header

---
+++ database/migrations/2023_05_14_062831_create_project_headers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_headers', function (Blueprint $table) {
+            $table->id();
+            $table->string('projectName');
+            $table->date('projectDueDate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_headers');
+    }
+};
