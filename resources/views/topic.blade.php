1b3036412c223cbefa6638bef6018b302a993f43
zton21 <kartono.apriliyandi@binus.ac.id>
2023-05-23 11:05:19 +0700

    topic done

--- resources/views/topic.blade.php
+++ resources/views/topic.blade.php
@@ -17,8 +17,8 @@
 </head>
 <body>
     <div class="container p-0">
-        <div class="row border border-dark">
-            <div class="col-5">
+        <div class="row">
+            <div class="col-5" style="border: 1px solid #A3A3A5;">
                 <div class="container-fluid p-0">
                     <div class="row card-bg p-1 align-items-end" style="height: 96px">
                         <div class="col-6"></div>
@@ -46,7 +46,7 @@
                             </a>
                         </div>
                     </div>
-                    <div class="row d-flex align-items-start">
+                    <div class="row d-flex align-items-start px-3">
                         <div class="col-6">
                             <h4 class="m-0">Brainstorming</h4>
                             <div class="text-secondary">10/1/2023 - 02.39 PM</div>
@@ -56,8 +56,8 @@
                             <div>by Gundul Pacul</div>
                         </div>
                     </div>
-                    <hr class="mt-1 mb-2">
-                    <div class="row d-flex align-items-center justify-content-between">
+                    <hr class="mt-1 mb-2 mx-3">
+                    <div class="row d-flex align-items-center justify-content-between px-3 pb-2">
                         <div class="col-12 d-flex flex-row align-items-center gap-2">
                             <img src="{{URL::asset('img/profilePicture.png')}}" alt="Profile Picture" class="img-fluid rounded-circle" style="width: 40px">
                             <div class="me-auto">I think a group work application is a good idea ...</div>
@@ -66,15 +66,84 @@
                     </div>
                 </div>
             </div>
-            <div class="col-7">
-                <div class="container-fluid">
-                    <div class="row">
-                        
+            <div class="col-7 p-0" style="border: 1px solid #A3A3A5;">
+                <div class="container-fluid p-0" style="background: #F3F8FE">
+                    <div class="row p-2 bg-white mx-0">
+                        <div class="col-12 d-flex flex-row gap-2">
+                            <h1>Design System</h1>
+                            <div class="mt-2 me-auto" class="text-secondary">10/2/2023 - 02.36 PM</div>
+                            <svg class="mt-1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256"><path fill="black" d="M128 96a32 32 0 1 0 32 32a32 32 0 0 0-32-32Zm0 48a16 16 0 1 1 16-16a16 16 0 0 1-16 16ZM48 96a32 32 0 1 0 32 32a32 32 0 0 0-32-32Zm0 48a16 16 0 1 1 16-16a16 16 0 0 1-16 16Zm160-48a32 32 0 1 0 32 32a32 32 0 0 0-32-32Zm0 48a16 16 0 1 1 16-16a16 16 0 0 1-16 16Z"/></svg>
+                        </div>
+                    </div>
+                    <div class="row px-2 mx-0 bg-white pb-3">
+                        <div class="col-12 d-flex flex-row gap-2 align-items-center">
+                            <img src="{{URL::asset('img/profilePicture.png')}}" alt="Profile Picture" class="img-fluid rounded-circle" style="width: 40px">
+                            <div class="me-auto fs-5">doloremque dolor est quia delectus non galisum culpa?</div>
+                        </div>
+                    </div>
+                    <hr class="p-0 my-0">
+                    <div class="container-fluid p-0">
+                        <div class="row px-4 py-2">
+                            <div class="col-1">
+                                <img src="{{URL::asset('img/profilePicture.png')}}" alt="Profile Picture" class="img-fluid rounded-circle">
+                            </div>
+                            <div class="col-11">
+                                <div class="d-flex flex-row gap-2">
+                                    <div>Gundul Pacul</div>
+                                    <div class="text-secondary" style="font-size: 12px">10/1/2023 - 02.39 PM</div>
+                                </div>
+                                <div class="p-2 bg-white rounded shadow d-inline-block text-break">
+                                    whatsup dude
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row px-4 py-2">
+                            <div class="col-1">
+                                <img src="{{URL::asset('img/profilePicture.png')}}" alt="Profile Picture" class="img-fluid rounded-circle">
+                            </div>
+                            <div class="col-11">
+                                <div class="d-flex flex-row gap-2">
+                                    <div>Gundul Pacul</div>
+                                    <div class="text-secondary" style="font-size: 12px">10/1/2023 - 02.39 PM</div>
+                                </div>
+                                <div class="p-2 bg-white rounded shadow d-inline-block text-break">
+                                    whatsup dude
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row px-4 py-2">
+                            <div class="col-11">
+                                <div class="d-flex flex-row-reverse gap-2">
+                                    <div>Pacul Tembak</div>
+                                    <div class="text-secondary" style="font-size: 12px">10/1/2023 - 02.39 PM</div>
+                                </div>
+                                <div class="d-flex flex-row-reverse">
+                                    <div class="p-2 rounded shadow d-inline-flex text-break" style="background: #D7E6FD">
+                                        whatsup dude
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-1">
+                                <img src="{{URL::asset('img/profilePicture.png')}}" alt="Profile Picture" class="img-fluid rounded-circle">
+                            </div>
+                        </div>
+                    </div>
+                    <hr class="p-0 my-0">
+                    <div class="row px-2 py-3 mx-0 bg-white pb-3 d-flex">
+                        <div class="col-12">
+                            <div class="d-flex flex-row gap-2 align-items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 5H6a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-2M8 5v0a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v0M8 5v0a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2v0"/></svg>
+                                <div class="input-group">
+                                    <input type="text" class="py-2 form-control" placeholder="Type your comment..." aria-describedby="basic-addon1">
+                                </div>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m5 12l-.604-5.437C4.223 5.007 5.825 3.864 7.24 4.535l11.944 5.658c1.525.722 1.525 2.892 0 3.614L7.24 19.465c-1.415.67-3.017-.472-2.844-2.028L5 12Zm0 0h7"/></svg>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    
+
 </body>
 </html>
