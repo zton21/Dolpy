3b10f03ab2e3072b6b2984fff7b28b17fe5d38ce
HelpMePliz <87262213+HelpMePliz@users.noreply.github.com>
2023-07-10 16:21:08 +0700

    Invite member working (?)

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -47,9 +47,8 @@ class ProjectController extends Controller
     public static function view_project(Request $request, $project_id) {
         $result = DB::select(
             "SELECT t.*, u.firstName, c.chatContent, (t.n_message - IFNULL(tu.seen, 0)) AS new_message 
-            FROM topic_sections t                     # data message terakhir
-            JOIN users u                                               # 
-            LEFT JOIN topic_user tu ON t.id = tu.topic_id AND u.id = tu.user_id     # data 
+            FROM topic_sections t JOIN users u                              
+            LEFT JOIN topic_user tu ON t.id = tu.topic_id AND u.id = tu.user_id 
             LEFT JOIN comments c ON t.last_comment_id = c.id   
             WHERE t.project_id = :project_id AND u.id = :user_id" 
         , ['user_id' => Auth::user()->id, 'project_id' => $project_id]);
@@ -86,6 +85,7 @@ class ProjectController extends Controller
 
     // Add member sebagai Member
     public static function add_member($project_id, $user_id) {
+        if (ProjectDetail::where('project_id', $project_id)->where('user_id', $user_id)->first()) return;
         $ProjectDetail = new ProjectDetail;
         $ProjectDetail->user_id = $user_id;
         $ProjectDetail->project_id = $project_id;
@@ -226,16 +226,17 @@ class ProjectController extends Controller
 
     public static function member_request_handler(Request $request, $id) {
         if ($request->has('task') && $request->task == 'invite') return ProjectController::invite_member($request, $id);
+        return response('Request not recognized', 200);
     }
 
     public static function invite_member(Request $request, $project_id) {
         if ($request->role->role != 'Creator') return response('Only creator can invite member', 403);
         
         // Langsung add :
-        $to_invite = User::where('email', $request->member_email);
+        $to_invite = User::where('email', $request->member_email)->first();
         if (!$to_invite) return redirect()->back()->withErrors(['member_email' => 'Member not found']);
 
         ProjectController::add_member($project_id, $to_invite->id);
-        return redirect::back()->with([['success' => 'Successfully invited member']]);
+        return redirect()->back()->with([['success' => 'Successfully invited member']]);
     }
 }
