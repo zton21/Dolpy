eb5fe79436f5434cf0286d81863f3e762000886d
zton21 <kartono.apriliyandi@binus.ac.id>
2023-07-10 13:39:47 +0700

    add timeline form

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -31,10 +31,11 @@
             <div class="col-6">
                 <div class="d-flex flex-row justify-content-between">
                     <h3>Timeline Dashboard</h3>
-                    <button type="button" class="btn btn-primary">
+                    <button onclick="openAddTimelineFormModal()" class="btn btn-primary">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="white" fill-rule="evenodd" clip-rule="evenodd"><path d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12Zm10-8a8 8 0 1 0 0 16a8 8 0 0 0 0-16Z"/><path d="M13 7a1 1 0 1 0-2 0v4H7a1 1 0 1 0 0 2h4v4a1 1 0 1 0 2 0v-4h4a1 1 0 1 0 0-2h-4V7Z"/></g></svg>
                         <span class="text-white my-auto">Add Timeline</span>
                     </button>
+                    <x-add-timeline-form></x-add-timeline-form>
                 </div>
                 <h5>Current completion:</h5>
                 <div class="d-flex flex-row justify-content-around">
