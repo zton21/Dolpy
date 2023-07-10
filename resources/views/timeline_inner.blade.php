827464c287bddd515f25de42ad915d9e7a5d4f80
wingorithm <erwin.gunawan@binus.ac.id>
2023-07-10 08:05:34 +0700

    Test comit new

--- resources/views/timeline_inner.blade.php
+++ resources/views/timeline_inner.blade.php
@@ -284,47 +284,7 @@
                         Add another task...</a>
                     </div>
                 </div>
-                <div class="card p-0 mx-3" style="width: 18rem;">
-                    <div class="card-header d-flex justify-content-between">
-                        <div>
-                            <h3 class="card-title">List Title</h3>
-                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M7.75879 12.25L9.00879 13.5L12.3421 10.1667" stroke="#379436" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                              <path d="M8.33366 5.00002H11.667C13.3337 5.00002 13.3337 4.16669 13.3337 3.33335C13.3337 1.66669 12.5003 1.66669 11.667 1.66669H8.33366C7.50033 1.66669 6.66699 1.66669 6.66699 3.33335C6.66699 5.00002 7.50033 5.00002 8.33366 5.00002Z" stroke="#379436" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-                              <path d="M13.3333 3.35004C16.1083 3.50004 17.5 4.52504 17.5 8.33337V13.3334C17.5 16.6667 16.6667 18.3334 12.5 18.3334H7.5C3.33333 18.3334 2.5 16.6667 2.5 13.3334V8.33337C2.5 4.53337 3.89167 3.50004 6.66667 3.35004" stroke="#379436" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-                              </svg>                    
-                              3/3
-                        </div>
-                        <a href="">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 256 256">
-                                <path fill="black" d="M128 96a32 32 0 1 0 32 32a32 32 0 0 0-32-32Zm0 48a16 16 0 1 1 16-16a16 16 0 0 1-16 16ZM48 96a32 32 0 1 0 32 32a32 32 0 0 0-32-32Zm0 48a16 16 0 1 1 16-16a16 16 0 0 1-16 16Zm160-48a32 32 0 1 0 32 32a32 32 0 0 0-32-32Zm0 48a16 16 0 1 1 16-16a16 16 0 0 1-16 16Z"></path>
-                            </svg>
-                        </a>
-                    </div>
-                    <ul class="list-group">
-                        <li class="list-group-item">
-                          <input class="form-check-input me-1" type="checkbox" value="" id="firstCheckbox">
-                          <label class="form-check-label" for="firstCheckbox">First checkbox</label>
-                        </li>
-                        <li class="list-group-item">
-                          <input class="form-check-input me-1" type="checkbox" value="" id="secondCheckbox">
-                          <label class="form-check-label" for="secondCheckbox">Second checkbox</label>
-                        </li>
-                        <li class="list-group-item">
-                          <input class="form-check-input me-1" type="checkbox" value="" id="thirdCheckbox">
-                          <label class="form-check-label" for="thirdCheckbox">Third checkbox</label>
-                        </li>
-                    </ul>
-                    <div class="card-footer">
-                        <a href="#" class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover">
-                        <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M7.33301 11H14.6663" stroke="#A3A3A5" stroke-width="1.375" stroke-linecap="round" stroke-linejoin="round"/>
-                            <path d="M11 14.6667V7.33333" stroke="#A3A3A5" stroke-width="1.375" stroke-linecap="round" stroke-linejoin="round"/>
-                            <path d="M8.24967 20.1667H13.7497C18.333 20.1667 20.1663 18.3333 20.1663 13.75V8.25C20.1663 3.66666 18.333 1.83333 13.7497 1.83333H8.24967C3.66634 1.83333 1.83301 3.66666 1.83301 8.25V13.75C1.83301 18.3333 3.66634 20.1667 8.24967 20.1667Z" stroke="#A3A3A5" stroke-width="1.375" stroke-linecap="round" stroke-linejoin="round"/>
-                            </svg>                        
-                        Add another task...</a>
-                    </div>
-                </div>
+                
                 <div class="card p-0 mx-3" style="width: 18rem; height:max-content">
                     <div class="card-footer">
                         <a href="#" class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover">
