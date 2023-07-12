68753fcb59b5c8f922c43566a614a7824f606def
shilviameidhi <shilvia.honova@binus.ac.id>
2023-07-12 14:13:16 +0700

    revision responsive faq page

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
 </head>
-<body>
+<body class="min-vh-100 d-flex flex-column">
     @include('layout.nav')
     @yield('content')
 </body>
