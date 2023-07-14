73da8eaf318bfd4017cfd45aaf7dd1551ef7c1fb
HelpMePliz <87262213+HelpMePliz@users.noreply.github.com>
2023-07-14 10:31:33 +0700

    dragndrop jquery

--- public/js/dragndorp.js
+++ public/js/dragndorp.js
@@ -1,42 +1,4 @@
-/* draggable element */
-let items = document.querySelectorAll('.task_card');
-
-items.forEach(item => {
-    item.addEventListener('dragstart', dragStart);
-});
-
-const toDo =  document.getElementById('todo');
-toDo.addEventListener('dragenter', dragEnter)
-toDo.addEventListener('dragover', dragOver);
-toDo.addEventListener('dragleave', dragLeave);
-toDo.addEventListener('drop', drop);
-
-const onProgress = document.getElementById("onprogress")
-onProgress.addEventListener('dragenter', dragEnter)
-onProgress.addEventListener('dragover', dragOver);
-onProgress.addEventListener('dragleave', dragLeave);
-onProgress.addEventListener('drop', drop);
-
-const done = document.getElementById("done")
-done.addEventListener('dragenter', dragEnter)
-done.addEventListener('dragover', dragOver);
-done.addEventListener('dragleave', dragLeave);
-done.addEventListener('drop', drop);
-
-function dragStart(e) {
-    e.dataTransfer.setData('element', e.target.id);
-    setTimeout(() => {
-        console.log('hide');
-        // e.target.classList.add('hide');
-    }, 0);
-}
-
 function dragEnter(e) {
-    e.preventDefault();
-    e.currentTarget.classList.add('drag-over');
-}
-
-function dragOver(e) {
     e.preventDefault();
     e.currentTarget.classList.add('drag-over');
 }
@@ -47,10 +9,23 @@ function dragLeave(e) {
 }
 
 function drop(e) {
-    console.log(3);
-    const id = e.dataTransfer.getData('element');
-    const draggable = document.getElementById(id);
-
-    e.currentTarget.classList.remove('drag-over');       
-    e.currentTarget.appendChild(draggable);
+    e.preventDefault();
+    e.currentTarget.classList.remove('drag-over');
+    const id = e.originalEvent.dataTransfer.getData('element');
+    $('#'+id).detach().appendTo(e.currentTarget);
 }
+
+$(() => {
+    $('.task_card').each(function() {
+        $(this).on('dragstart', function(e) {
+            e.originalEvent.dataTransfer.setData('element', e.target.id);
+        })
+    })
+    for (let x of ['todo', 'onprogress', 'done']) {
+        $('#'+x).on('dragenter', dragEnter);
+        $('#'+x).on('dragover', dragEnter);
+        $('#'+x).on('dragleave', dragLeave);
+        $('#'+x).on('drop', drop);
+        console.log('#'+x)
+    }
+});
