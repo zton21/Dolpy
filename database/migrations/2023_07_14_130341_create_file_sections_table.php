9a9a4358a72c5c8e5082a00f5f3583d57b53e3f0
zton21 <kartono.apriliyandi@binus.ac.id>
2023-07-14 13:07:21 +0700

    file section database

---
+++ database/migrations/2023_07_14_130341_create_file_sections_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('file_sections', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('project_id');
+            $table->foreignId('user_id');
+            $table->string('fileSectionName');
+            $table->date('fileSectionDate');
+            $table->timestamps();
+
+            $table->foreign('project_id')->references('id')->on('project_headers')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('file_sections');
+    }
+};
