2d4a8a57254c465cffd65bb97edc3b7d4040957b
HelpMePliz <87262213+HelpMePliz@users.noreply.github.com>
2023-07-17 08:38:03 +0700

    note model n migration

---
+++ database/migrations/2023_07_17_083453_create_notes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('title');
+            $table->'completed';
+            $
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+};
