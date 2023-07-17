540d05f6d0398638031f2918ec9ee508d972d06a
HelpMePliz <87262213+HelpMePliz@users.noreply.github.com>
2023-07-17 14:24:26 +0700

    Timeline inner

--- resources/views/topic.blade.php
+++ resources/views/topic.blade.php
@@ -60,13 +60,7 @@
             display: -webkit-box;
         }
     </style>
-    <script>
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-    </script>
+    <script>$.ajaxSetup({headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}});</script>
     <script src="{{asset('js/project.js')}}"></script>
 </head>
 <body>
